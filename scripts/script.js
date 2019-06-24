cb684456d1b79fc09e83a9cc09e9a67fbbdd1077
unknown <saadtaj96@gmail.com>
2019-06-24 14:48:38 -0400

    Added Projects Section

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
-function revealMessage(){
-	document.getElementById("hiddenMessage").style.display = 'block';
-}
-function countDown(){
-	var currentVal = document.getElementById("countDown").innerHTML;
-	var newVal = 0;
-	if (currentVal > 0) {
-		newVal = currentVal - 1 ;
+$(document).ready(function () {
+	var slide_1 = document.getElementById("#S_invaders");
+	var slide_2 = document.getElementById("#N_Battle");
+	var slide_3 = document.getElementById("#Mobile_game");
+	var currentIndex = 0;
+})
+$('#Carousel-Projects').bind('slid', function() {
+	
+	currentIndex = $('div.active').index() + 1;
+	if(currentIndex==0){
+		slide_1.style.visibility = "visible";
+		slide_2.style.visibility = "hidden";
+		slide_3.style.visibility = "hidden";
 	}
-	document.getElementById("countDown").innerHTML = newVal;
-}
+	else if(currentIndex==1){
+		slide_1.style.visibility = "hidden";
+		slide_2.style.visibility = "visible";
+		slide_3.style.visibility = "hidden";
+	}
+	else if(currentIndex==2){
+		slide_1.style.visibility = "hidden";
+		slide_2.style.visibility = "hidden";
+		slide_3.style.visibility = "visible";
+	}
+});

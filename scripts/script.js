04a0a850ab525344a9c6404e4ffed4846537b1aa
unknown <saadtaj96@gmail.com>
2019-07-11 20:08:08 -0400

    Corrected smooth scroll and scrollspy offsets

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,12 @@
-var navheight = 
-
-$(document).ready(function(){
-  // Add scrollspy to <body>
-  // Add smooth scrolling on all links inside the navbar
-  $("#navbarContent a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+$(function() {
+  var navHeight = $('.navbar').height(); // get height of navbar
+  $('li a').click(function (e) {
+      e.preventDefault();
+      var target = $(this.hash);
       $('html, body').animate({
-        scrollTop: $(hash).offset().navbar
-      }, 40, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
+          scrollTop: target.offset().top - navHeight // scroll to link minus height of navbar
+      }, 250);
+      window.location.hash = this.hash;
+      return false;
   });
 });

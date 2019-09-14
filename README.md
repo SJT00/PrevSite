f2436790db344c47f845060eb028c52c8b105b16
SJT00 <saadtaj96@gmail.com>
2019-09-13 21:03:31 -0400

    Added animation for first section, along with adding playable/downloadable game files.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 A portfolio website, to catalog my projects, and developing skills.
 Made using HTML, CSS, Jquery, Bootstrap, and Javascript.
+Visit here: [http://sjt00.github.io](http://sjt00.github.io)
---
+++ js/mainscript.js
@@ -0,0 +1,49 @@
+function navscrollto(link){// scrolls to link minus height of navbar
+  var navHeight = Math.round($(".nav").height());
+  $("html, body").animate(
+  { 
+       scrollTop: link.offset().top - navHeight - 17
+     }, 250);
+}
+
+function pglinks(){  
+  $("a").filter(".onpage").click(function (e) {// change scroll behaviour of all <a> tags with onpage class
+    e.preventDefault();
+    navscrollto($(this.hash));
+    window.location.hash = this.hash;
+  });
+}
+
+function playgame(){
+ $("#gameimg").hide();
+ $("#gamewv").show();
+ $('.carousel').carousel('pause');
+}
+
+// On Element load function
+$(function() {
+  pglinks();
+  if(document.URL.indexOf("#gamewv") >= 0){// Navigate user to playable game screen
+    navscrollto($("#Projects"));
+    $(".carousel").carousel(1);
+    playgame();
+  }
+  $("#play").click(function (){
+    if($("div.active").index() == 0){
+      window.location.href = "./assets/Games/S_invaders.zip";
+      $("#play").html("Enjoy!");
+    }
+    else if($("div.active").index() == 1){
+      $("#play").html("Go Ahead!");
+      playgame();
+    }
+    else if($("div.active").index() == 2){
+      $("#play").html("Me Too!㋡");
+    }
+  });
+  $(".carousel").on("slide.bs.carousel", function () {
+    $("#play").html("Want To Play!");
+    $("#gameimg").show();
+    $("#gamewv").hide();
+  });
+});
